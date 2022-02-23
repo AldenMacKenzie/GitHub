fb9384bee3567c8bd6535683b4f529f168d51dc4
ncacioppo <ndcacioppo@gmail.com>
2022-02-23 09:49:13 -0500

    Committed 9:47 AM 2/23/22

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,11 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+import java.util.Scanner;
 
+public class HelloWorld {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is your name?");
+        String name = input.next();
+        System.out.println("\nHello " + name);
+    }
 }
